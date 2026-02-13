30e06e43bfbe2c6bea31ebe09011d56d3d6f0aff
Akanksha0802-ss <akankshasingh91259@gmail.com>
2026-02-13 16:14:46 +0530

    jiraboard miniproject lecture-3

--- module-4/jiraboard/src/App.jsx
+++ module-4/jiraboard/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import TaskForm from "./components/TaskForm/task-form";
 import "./app.css";
 const App = () => {
   return (
     <div className="app">
-      <header className="app_header">Jira Board</header>
+      <h1 style={{textAlign: "center", margin:"50px"}}>Jira Board</h1>
+      <TaskForm />
       <main className="app_main">
         <section className="task_column">Section 1</section>
         <section className="task_column">Section 1</section>
