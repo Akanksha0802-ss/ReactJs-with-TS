b03b5b6dc153b611246bb6db4de69268cca9e7be
Akanksha0802-ss <akankshasingh91259@gmail.com>
2026-02-14 00:57:09 +0530

    jiraboard miniproject lecture-4 updated TaskForm components

--- module-4/jiraboard/src/components/TaskForm/task-form.jsx
+++ module-4/jiraboard/src/components/TaskForm/task-form.jsx
@@ -5,17 +5,24 @@ const TaskForm = () => {
     <header className="app_header">
         <form>
             <input type="text" className="task_input" placeholder="Enter task details"/>
-            <div className="">
-                <button className="tag">DEV</button>
-                <button className="tag">QA</button>
-                <button className="tag">Product Owner</button>
-            </div>
-            <select className="task_status">
+            <div className="task_form_bottom">
+                <div>
+                    <button className="tag">DEV</button>
+                    <button className="tag">QA</button>
+                    <button className="tag">Product Owner</button>
+                </div>
+                <div>
+                <select className="task_status">
                 <option value="Ready for Development">Ready for Development</option>
                 <option value="In Progress">In Progress</option>
                 <option value="Ready for test">Ready for test</option>
                 <option value="Closed">Closed</option>
-            </select>
+                </select>
+                <button type="submit" className="task_submit">
+                    + Add
+                </button>
+                </div>
+            </div>
         </form>
     </header>
   )
