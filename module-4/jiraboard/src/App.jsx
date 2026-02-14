56284b9070b719b10e1e2dac9081cd77b802a151
Akanksha0802-ss <akankshasingh91259@gmail.com>
2026-02-14 10:49:24 +0530

    lecture-7 create column component with styling

--- module-4/jiraboard/src/App.jsx
+++ module-4/jiraboard/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import TaskForm from "./components/TaskForm/task-form";
+import TaskColumn from "./components/TaskColumn/task-column";
+import closedIcon from "./assets/check-mark-button.png"
 import "./app.css";
 const App = () => {
   return (
@@ -7,10 +9,11 @@ const App = () => {
       <h1 style={{textAlign: "center", margin:"50px"}}>Jira Board</h1>
       <TaskForm />
       <main className="app_main">
-        <section className="task_column">Section 1</section>
-        <section className="task_column">Section 1</section>
-        <section className="task_column">Section 1</section>
-        <section className="task_column">Section 1</section>
+          <TaskColumn title="Ready for Development" />
+          <TaskColumn title="In Progress" />
+          <TaskColumn title="Ready for test" />
+          <TaskColumn title="Closed" />
+          <TaskColumn title="Closed" icon={closedIcon} />
       </main>
     </div>
   )
